cfce1134dde118b66b6c85524be9e4caf9284319
lcabelardo12-lang <lcabelardo12@gmail.com>
2026-07-22 14:02:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="duava-ops-v7";
+var CACHE="duava-ops-v8";
 var URLS=["./index.html","./manifest.json"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.filter(function(x){return x!==CACHE}).map(function(x){return caches.delete(x)}))}))});
